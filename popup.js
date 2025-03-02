4279b2c51196d17d85831f96394e84970a7b4185
Davit <64492505+DavitJanashia@users.noreply.github.com>
2025-03-02 16:20:06 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -69,6 +69,9 @@ function demoSetUpAuth() {
       } else {
         gotoUnauthenticatedState();
       }
+    })
+   .catch(function(error) {
+      console.error('Error during authentication:', error);
     });
 }
 
